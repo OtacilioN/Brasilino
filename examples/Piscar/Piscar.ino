90cda90272436960aab85b20f1a99db2f717111f
ThiagoAugustoMartins <martinsthiagoaugusto@gmail.com>
2016-07-07 17:17:31 -0300

    Esqueleto atualizado 0.1

---
+++ examples/Piscar/Piscar.ino
@@ -0,0 +1,14 @@
+#include <Brasilino.h>
+
+/*
+    Piscar
+    Liga um LED por um segundo, depois desliga-o, repetidamente.
+    
+    Este exemplo de código está em domínio público.
+    
+    modificado em 07 de julho de 2014
+    Desenvolvido por Otacilio Neto e Thiago Augusto Martins.
+    Orientado pelo Prof. Dr. Rômulo Cesar Carvalho de Araújo.
+    
+    Este exemplo é baseado no exemplo Blink do Arduino.
+*/    
--- examples/lerSerial/lerSerial.ino
+++ examples/lerSerial/lerSerial.ino
@@ -1,1 +1,17 @@
 #include <Brasilino.h>
+
+/*
+    lerSerial
+    Lê a entrada analógica 0, e mostra o resultado no monitor serial.
+    
+    Conecte o pino central do potenciômetro ao pino Analógico 0, e os pinos de alimentação aos
+    pinos +5V e GND.
+    
+    Este exemplo de código está em domínio público.
+    
+    modificado em 07 de julho de 2014
+    Desenvolvido por Otacilio Neto e Thiago Augusto Martins.
+    Orientado pelo Prof. Dr. Rômulo Cesar Carvalho de Araújo.
+    
+    Este exemplo é baseado no exemplo AnalogReadSerial do Arduino.
+*/    
