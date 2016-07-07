df4025acb2531d0fc73818693373e28873fb4a38
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2016-07-07 17:55:27 +0000

    Esqueleto dos arquivos .h e .cpp adicionados.

---
+++ Brasilino.h
@@ -0,0 +1,14 @@
+
+/*
+	Brasilino.h - Programe em PT-BR.
+	Esta biblioteca insere uma série de facilitações e permite programar em linguagem Arduino utilizando como lingua nativa o português do Brasil.
+	Mais instruções disponíveis em: https://github.com/OtacilioN/Brasilino
+	Desenvolvido por Otacilio Neto e Thiago Augusto Martins, Julho, 2016.
+	Orientado pelo Prof. Dr. Rômulo Cesar Carvalho de Araújo.
+*/
+
+#ifndef Brasilino_h
+#define Brasilino_h
+
+
+#endif
