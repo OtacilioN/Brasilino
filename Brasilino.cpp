1966cc328b49be39decd89607b075037401a930d
Otacilio Neto <otacilio.n97@gmail.com>
2016-07-21 19:08:38 +0000

    Parametro da cpp traduzido

--- Brasilino.cpp
+++ Brasilino.cpp
@@ -1,9 +1,9 @@
 #include "Brasilino.h"
 #include <math.h>
 
-dobro temperatura(int RawADC) {
+dobro temperatura(int valorAnalogico) {
  dobro Temp;
- Temp = log(10000.0*((1024.0/RawADC-1))); 
+ Temp = log(10000.0*((1024.0/valorAnalogico-1))); 
  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
  Temp = Temp - 273.15;  
  retorne Temp;
