44487db03d16332e52931df3f92a53cbe94f551b
Erick Simões <erick.simoes@live.com>
2017-02-25 01:00:54 -0300

    fix: Adoção de tipos nativos no código intetno da biblioteca
    Resolves: #17

--- src/Brasilino.cpp
+++ src/Brasilino.cpp
@@ -22,41 +22,40 @@ void iniciarSerial(int baud) {
 	Serial.begin(baud);
 }
 
-dobro temperatura(inteiro valorAnalogico) {
-	dobro temp;
-	temp = log(10000.0 * (1024.0 / valorAnalogico - 1));
+double temperatura(int valorAnalogico) {
+	double temp = log(10000.0 * (1024.0 / valorAnalogico - 1));
 	temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temp * temp )) * temp);
 	temp = temp - 273.15;
-	retorne temp;
+	return temp;
 }
 
 /* ULTRASSOM */
 Ultrassom::Ultrassom(int tp, int ep) {
-	saida(tp);
-	entrada(ep);
+	pinMode(tp, OUTPUT);
+	pinMode(ep, INPUT);
 	trig_pino = tp;
 	echo_pino = ep;
 	tempo_limite = 3000;  // 3000 µs = 50cm // 30000 µs = 5 m // Configurado para funcionar ate 50 cm.
 }
 
 long Ultrassom::Temporizando() {
-	desligar(trig_pino);
+	digitalWrite(trig_pino, LOW);
 	delayMicroseconds(2);
-	ligar(trig_pino);
+	digitalWrite(trig_pino, HIGH);
 	delayMicroseconds(10);
-	desligar(trig_pino);
+	digitalWrite(trig_pino, LOW);
 	
 	duracao = pulseIn(echo_pino, HIGH, tempo_limite);
 	
 	se(duracao == 0)
 		duracao = tempo_limite;
 
-	retorne duracao;
+	return duracao;
 }
 
 long Ultrassom::Medir() {
 	Temporizando();
 	distancia = duracao / 29 / 2;
-	retorne distancia;
+	return distancia;
 }
 // Não apagar antes disso
