39f5b451c75bf4c3ddb8d86269b7b5b1c057846e
Otacilio Neto <otacilio.n97@gmail.com>
2017-02-27 13:19:05 +0000

    docs: Dicas adicionadas no inicio de quarentaedois.ino
    Dicas sobre comentários, sintaxe e Case-sensitive, com uma pitada de humor, adicionados ao inicio do arquivo.

--- examples/Intermediario/quarentaedois/quarentaedois.ino
+++ examples/Intermediario/quarentaedois/quarentaedois.ino
@@ -9,6 +9,22 @@
     
 */    
 
+// Dica: Você pode criar uma linha de comentário utilizando duas barras no inicio de uma linha
+
+/* Dica: Caso o seu comentário possua mais de uma linha, você pode utilizar um / seguido de um *,
+         e ao final das linhas do seu comentário é só fechar com * seguido de um /, assim tudo
+         que estiver dentro das linhas de comentário não será verificado pelo compilador */
+         
+/* Dica: A palavra Sintaxe, em programação, refere-se a forma com que as instruções são escritas, você 
+         pode conferir se a sintaxe de uma instrução está correta pela mudança de cor, todas as instruções 
+         do Brasilino possuem uma cor específica para indicar que foram digitadas corretamente /*
+
+/* Dica: No Brasilino, assim como em Arduino, as letras MAIÚSCULAS e minúsculas fazem diferença na sintaxe de 
+         uma instrução, portanto a instrução ligar(); é interpretada em Brasilino, mas a instrução Ligar, não.
+         Em inglês, chamamos esta diferenciação de maíusculos e minúsculos de "Case-sensitive" e não "Case-Sensitive" */
+         
+
+
 // Inclue a biblioteca Brasilino
 #include <Brasilino.h>
 
