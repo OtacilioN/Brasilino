c7eb3b5b69de726815fa101c9eaaac48c8eb3513
Otacilio Neto <otacilio.n97@gmail.com>
2017-02-27 01:27:03 +0000

    feat: quarentaedois.ino adicionado
    Arquivo de exemplo quarentaedois.ino adicionado na pasta de examples > Intermediario, este arquivo deve conter um teste de todas as instrucoes implementadas

---
+++ examples/Intermediario/quarentaedois/quarentaedois.ino
@@ -0,0 +1,23 @@
+/*
+    quarentaedois
+    Programa exemplo que contém a respostam para a vida, o universo e tudo mais.
+    
+    Este exemplo de código está em domínio público.
+    
+    criado em 26 de fevereiro de 2016
+    Desenvolvido por Otacilio Neto.
+    
+*/    
+
+// Inclue a biblioteca Brasilino
+#include <Brasilino.h>
+
+// Você também pode chamar de proedimento configurar se preferir
+funcao configurar(){
+
+}
+
+// Aqui também pode ser chamado de procedimento repetir, fica à sua escolha
+funcao repetir() {
+    
+}
