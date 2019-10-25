5ee8bd939a998513f5f5c68ad666a98cc5b4a24d
Ladson <lgs3@live.cin.ufpe.br>
2019-10-25 15:23:23 -0300

    Adicionado código exemplo ASCII

---
+++ examples/Intermediario/ASCII/ASCII.ino
@@ -0,0 +1,73 @@
+/*
+  Tabela ASCII
+
+  Imprime os valores dos bytes nos seguintes formatos:
+  - como decimal codificado em ASCII, hexadecimal, octal e binário
+
+  Para mais sobre ASCII, veja https://pt.wikipedia.org/wiki/ASCII
+
+  Não necessita circuito ou hardware adicional.
+
+  Codado em Brasilino em outubro de 2019
+  por Ladson Gomes (github.com/ladsong)
+  Baseado no código de exemplo da biblioteca arduino
+  por Nicholas Zambetti <http://www.zambetti.com>
+
+*/
+#include <Brasilino.h>
+
+funcao configurar() {
+  //Inicializar serial, e definir o baudrate de comunicação
+  iniciarSerial(9600);
+
+
+  // escreve o titulo com quebra de linha no final
+  escreverSerialn("Mapa de caracteres ~ Tabela ASCII");
+}
+
+// primeiro caracter visivel em ASCII '!' é o numero 33:
+inteiro esteByte = 33;
+// você também pode escrever caracteres em ASCII entre aspas simples.
+// por exemplo, '!' é o mesmo que 33, então você também poderia usar isso:
+// inteiro esteByte = '!';
+
+funcao repetir() {
+  // prints value unaltered, i.e. the raw binary version of the byte.
+  // The Serial Monitor interprets all bytes as ASCII, so 33, the first number,
+  // will show up as '!'
+  escreverSerial(esteByte);
+
+  escreverSerial(", dec: ");
+  // escreve o valor como uma string codificada em ASCII em decimal ( base 10).
+  // Decimal é o formato padrão para as funções escreverSerial() e escreverSerialn().
+  // então nenhum modificador é necessário:
+
+  escreverSerial(esteByte);
+  // Mas você pode declarar o modificador para decimal, se quiser.
+  // dessa forma também funciona se você descomentar:
+
+  // escreverSerial(esteByte, DEC);
+
+
+  escreverSerial(", hex: ");
+  // escreve o valor como uma cadeia de caracteres em hexadecimal (base 16):
+  escreverSerial(esteByte, HEX);
+
+  escreverSerial(", oct: ");
+  // escreve o valor como uma cadeia de caracteres em octal (base 8)
+  escreverSerial(esteByte, OCT);
+
+  escreverSerial(", bin: ");
+  // escreve o valor como uma cadeia de caracteres binária(base 2) junto com a quebra de final de linha:
+  escreverSerialn(esteByte, BIN);
+
+  // se imprimir o ultimo caracter visivel '~' ou 126, pare:
+  se (esteByte == 126) {    // também é válido usar se(esteByte == '~') {
+    // Fica para sempre nesse loop
+    enquanto (verdadeiro) {
+      continue;
+    }
+  }
+  // avança para o proximo caracter
+  esteByte++;
+}
