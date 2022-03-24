a5b04cfac600d1bc47648db7ac754a35ce779d46
Raphael Rebello de Godoy <57528077+rgodoy86@users.noreply.github.com>
2022-03-24 13:15:47 +0100

    :art: formatacao atualizada do codigo fonte
    Corrigido a formatacao das chaves { } e tabulacao.

--- src/Brasilino.cpp
+++ src/Brasilino.cpp
@@ -17,20 +17,17 @@
 #if defined(ARDUINO_AVR_GEMMA)
 #elif defined(ARDUINO_AVR_DIGISPARK)
 #else
-void iniciarSerial(void)
-{
+void iniciarSerial(void){
 	Serial.begin(9600);
 }
 
-void iniciarSerial(int baud)
-{
+void iniciarSerial(int baud){
 	Serial.begin(baud);
 }
 
 #endif
 
-double temperatura(int valorAnalogico)
-{
+double temperatura(int valorAnalogico){
 	double temp = log(10000.0 * (1024.0 / valorAnalogico - 1));
 	temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temp * temp)) * temp);
 	temp = temp - 273.15;
@@ -38,8 +35,7 @@ double temperatura(int valorAnalogico)
 }
 
 /* ULTRASSOM */
-Ultrassom::Ultrassom(int tp, int ep)
-{
+Ultrassom::Ultrassom(int tp, int ep){
 	pinMode(tp, OUTPUT);
 	pinMode(ep, INPUT);
 	trig_pino = tp;
@@ -47,8 +43,7 @@ Ultrassom::Ultrassom(int tp, int ep)
 	tempo_limite = 3000; // 3000 µs = 50cm // 30000 µs = 5 m // Configurado para funcionar ate 50 cm.
 }
 
-long Ultrassom::Temporizando()
-{
+long Ultrassom::Temporizando(){
 	digitalWrite(trig_pino, LOW);
 	delayMicroseconds(2);
 	digitalWrite(trig_pino, HIGH);
@@ -63,8 +58,7 @@ long Ultrassom::Temporizando()
 	return duracao;
 }
 
-long Ultrassom::Medir()
-{
+long Ultrassom::Medir(){
 	Temporizando();
 	return duracao / 29 / 2;
 }
@@ -76,8 +70,8 @@ Motor::Motor(uint8_t pino_A, uint8_t pino_B, uint8_t pino_pwm) {
 	_pino_pwm = pino_pwm;
 
 	pinMode(_pino_A, OUTPUT);
-  pinMode(_pino_B, OUTPUT);
-  pinMode(_pino_pwm, OUTPUT);
+  	pinMode(_pino_B, OUTPUT);
+  	pinMode(_pino_pwm, OUTPUT);
 }
 
 void Motor::configurarVelocidade(uint8_t velocidade) {
@@ -92,11 +86,11 @@ void Motor::girar(uint8_t sentido) {
 	switch (_sentido) {
 		case PARA_FRENTE:
 			digitalWrite(_pino_A, HIGH);
-	    digitalWrite(_pino_B, LOW);
+	    		digitalWrite(_pino_B, LOW);
 			break;
 		case PARA_TRAS:
 			digitalWrite(_pino_A, LOW);
-	    digitalWrite(_pino_B, HIGH);
+	    		digitalWrite(_pino_B, HIGH);
 			break;
 		default:
 		break;
