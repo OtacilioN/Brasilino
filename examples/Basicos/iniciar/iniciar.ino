cecbf9ec32c1c268cd92ac6e3c85bc36a69d390c
Erick Simões <erick.simoes@live.com>
2025-09-19 16:26:00 -0300

    Cria um código inicial vazio de template
    Este exemplo fica disponível no menu Arquivo -> Exemplos -> Brasilino -> Basicos -> iniciar

---
+++ examples/Basicos/iniciar/iniciar.ino
@@ -0,0 +1,11 @@
+#include <Brasilino.h>
+
+funcao configurar() {
+  // coloque seu código de configuração aqui, para ser executado uma vez:
+  
+}
+
+funcao repetir() {
+  // coloque seu código principal aqui, para ser executado repetidamente:
+  
+}
